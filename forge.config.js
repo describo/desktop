7ca1a9c2a690df03c9904ce436261319873bf0fb
Marco La Rosa <m@lr.id.au>
2023-01-03 12:40:26 +1100

    enable osx signing and notarizing

--- forge.config.js
+++ forge.config.js
@@ -10,23 +10,23 @@ module.exports = {
             "postcss.config.js",
             "tailwind.config.js",
             "README.md"
-        ]
-        // osxSign: {
-        //     optionsForFile: (filePath) => {
-        //         // Here, we keep it simple and return a single entitlements.plist file.
-        //         // You can use this callback to map different sets of entitlements
-        //         // to specific files in your packaged app.
-        //         return {
-        //             entitlements: "./build/entitlements.mac.plist"
-        //         }
-        //     }
-        // },
-        // osxNotarize: {
-        //     tool: "notarytool",
-        //     appleId: process.env.APPLE_ID,
-        //     appleIdPassword: process.env.APPLE_PASSWORD,
-        //     teamId: process.env.APPLE_TEAM_ID
-        // }
+        ],
+        osxSign: {
+            optionsForFile: (filePath) => {
+                // Here, we keep it simple and return a single entitlements.plist file.
+                // You can use this callback to map different sets of entitlements
+                // to specific files in your packaged app.
+                return {
+                    entitlements: "./build/entitlements.mac.plist"
+                }
+            }
+        },
+        osxNotarize: {
+            tool: "notarytool",
+            appleId: process.env.APPLE_ID,
+            appleIdPassword: process.env.APPLE_PASSWORD,
+            teamId: process.env.APPLE_TEAM_ID
+        }
     },
     rebuildConfig: {},
     makers: [
